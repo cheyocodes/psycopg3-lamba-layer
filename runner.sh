b4cd870b1e0d8b0f33ef1a25d2d015044d8918e2
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-03-21 22:21:42 -0500

    update runner

--- runner.sh
+++ runner.sh
@@ -1,11 +1,12 @@
-#! /usr/bin/bash
+#! /bin/bash
 
-container_name="lambda_docker"
+container_name="psycopg3_lambda_layer_container"
 docker_image="aws_lambda_builder_image"
 artifact_directory="psycopg3"
 
 echo $container_name
 echo $docker_image
+
 
 # Builds a Docker image with the specified parameters.
 docker buildx build -t $docker_image --platform "linux/x86_64" .
@@ -27,7 +28,7 @@ docker stop $container_name
 
 echo "removing image..."
 
-sleep 5
+sleep 3
 
 # Removes the specified Docker image.
 docker image rm $docker_image:latest
@@ -39,3 +40,5 @@ sleep 3
 # creates a directory and moves the python.zip artifact in there
 mkdir $artifact_directory
 mv python.zip $artifact_directory
+
+printf "\nFinished!\n"
